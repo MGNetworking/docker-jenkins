637560a0e4c76a46511caa35f6ed596e7445f184
Maxime <ghalem.maxime@gmail.com>
2024-01-26 17:11:42 +0100

    Update: création de nouveau script permettent de créer l'images est de l'ajouter au depot personnlisé.

--- script/build_push.sh
+++ script/build_push.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-# Script récupérer l'images du dépôt et lance le conteneur
+# Ce scipt permet de créer l'images jenkins_ops
+# puis l'ajoute au dépôt
 
 # Définir le nom du dépôt
 DOCKER_REGISTRY=sonatype-nexus.backhole.ovh
+
+# exécution du script dans l'environnement en cours
+. ./script/docker_id.sh
+
+# Importation de l'image utiliser dans le dockerfile
+# Dans le but d'éviter l'import en cache
+docker pull jenkins/jenkins:lts-jdk17
+
+# Construire l'image
+docker compose build --no-cache
 
 # Demander à l'utilisateur de saisir le mot de passe
 read -p "Veuillez saisir votre nom d'utilisateur de votre depot Docker: " DOCKER_USERNAME
@@ -12,20 +23,8 @@ echo # Ajouter un saut de ligne pour une meilleure présentation
 # connexion au dépot
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" "$DOCKER_REGISTRY"
 
-# Pull de l'images en correspondance au docker compose cible
-docker compose pull
+# Pousser l'image vers le dépôt personnel
+docker compose push
 
 # déconnexion du dépôt
 docker logout "$DOCKER_REGISTRY"
-
-# Création du dossier /jenkins_home avec les permissions appropriées
-sudo mkdir jenkins_home
-
-# modif jenkins dossier
-sudo chown -R 1000:1000 jenkins_home/
-
-# Démarrage du conteneur Jenkins avec Docker Compose
-docker compose up -d
-
-# voir les logs
-docker compose logs -f
